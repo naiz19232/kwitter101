aecc3dde9e490019b348f8e45c3dcb618feee83d
naiz19232 <33213878+naiz19232@users.noreply.github.com>
2022-01-12 18:54:29 +0530

    Update kwitter_page.js

--- kwitter_page.js
+++ kwitter_page.js
@@ -10,21 +10,7 @@ var firebaseConfig = {
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-	user_name = localStorage.getItem("user_name");
-	room_name = localStorage.getItem("room_name");
 
-function send()
-{
-  msg = document.getElementById("msg").value;
-  firebase.database().ref(room_name).push({
-    name:user_name,
-    message:msg,
-    like:0
-   });
-
-  document.getElementById("msg").value = "";
-}
 
 function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
          firebase_message_id = childKey;
@@ -46,22 +32,4 @@ like_button ="<button class='btn btn-warning' id="+firebase_message_id+" value="
       } });  }); }
 getData();
 
-function updateLike(message_id)
-{
-  console.log("clicked on like button - " + message_id);
-	button_id = message_id;
-	likes = document.getElementById(button_id).value;
-	updated_likes = Number(likes) + 1;
-	console.log(updated_likes);
 
-	firebase.database().ref(room_name).child(message_id).update({
-		like : updated_likes  
-	 });
-
-}
-
-function logout() {
-localStorage.removeItem("user_name");
-localStorage.removeItem("room_name");
-window.location.replace("kwitter.html");
-}
