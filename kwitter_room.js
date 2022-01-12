1b91f896e8db05041cc9d662261a63dc94fe5ba4
naiz19232 <33213878+naiz19232@users.noreply.github.com>
2022-01-12 18:49:09 +0530

    Update kwitter_room.js

--- kwitter_room.js
+++ kwitter_room.js
@@ -12,7 +12,7 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 //ADD YOUR FIREBASE LINKS
 
-  user_name = localStorage.getItem("user_name");
+ 
 
 document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
 
